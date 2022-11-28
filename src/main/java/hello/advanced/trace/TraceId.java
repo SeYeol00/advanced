722b4d487c6b91d11738ee8e30b8228e19cd8e1d
SeYeol_Park <79959576+SeYeol00@users.noreply.github.com>
2022-11-28 18:14:54 +0900

    11/28

--- src/main/java/hello/advanced/trace/TraceId.java
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -1,2 +1,30 @@
-package hello.advanced.trace;public class TraceId {
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+
+    private int level;
+
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    public TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        // UUID를 생성하여 앞 8자리만 사용
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+
+    private TraceId createNextId(){
+        return new TraceId(id,level+1);
+    }
 }
